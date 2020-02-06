05c3b321a2d60473e795de5db8b72de6ceb08c09
Vladimir Ventura <vladventura@outlook.es>
2020-02-06 11:30:59 -0500

    Template for the different screens of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,109 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  // Over here we would wrap the Application with the
+  // ChangeNotifierProvider widget, or a MultiProvider widget
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  AppState createState() => new AppState();
+}
+
+class AppState extends State<App> {
+  int bottomNavBarIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("SSBU Info App Main Screen Template"),
+        ),
+        body: chooseDisplay(bottomNavBarIndex),
+        bottomNavigationBar: new BottomNavigationBar(
+          items: [
+            new BottomNavigationBarItem(
+                icon: Icon(Icons.ac_unit), title: new Text("Characters")),
+            new BottomNavigationBarItem(
+                icon: Icon(Icons.ac_unit), title: new Text("Stages")),
+            new BottomNavigationBarItem(
+                icon: Icon(Icons.ac_unit), title: new Text("Music")),
+          ],
+          currentIndex: bottomNavBarIndex,
+          onTap: (int tappedIndex) {
+            setState(() {
+              bottomNavBarIndex = tappedIndex;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Widget chooseDisplay(int index) {
+  switch (index) {
+    case 0:
+      return charactersDisplay();
+      break;
+    case 1:
+      return musicDisplay();
+      break;
+    case 2:
+      return stagesDisplay();
+      break;
+    default:
+      break;
+  }
+}
+
+ListView charactersDisplay() {
+  return ListView(
+    children: <Widget>[
+      new Text("Character 1"),
+      new Text("Character 2"),
+      new Text("Character 3"),
+      new Text("Character 4"),
+      new Text("Character 5"),
+      new Text("Character 6"),
+      new Text("Character 7"),
+      new Text("Character 8"),
+      new Text("Character 9"),
+      new Text("Character 10"),
+    ],
+  );
+}
+
+ListView musicDisplay() {
+  return ListView(
+    children: <Widget>[
+      new Text("Music 1"),
+      new Text("Music 2"),
+      new Text("Music 3"),
+      new Text("Music 4"),
+      new Text("Music 5"),
+      new Text("Music 6"),
+      new Text("Music 7"),
+      new Text("Music 8"),
+      new Text("Music 9"),
+      new Text("Music 10"),
+    ],
+  );
+}
+
+ListView stagesDisplay() {
+  return ListView(
+    children: <Widget>[
+      new Text("Stage 1"),
+      new Text("Stage 2"),
+      new Text("Stage 3"),
+      new Text("Stage 4"),
+      new Text("Stage 5"),
+      new Text("Stage 6"),
+      new Text("Stage 7"),
+      new Text("Stage 8"),
+      new Text("Stage 9"),
+      new Text("Stage 10"),
+    ],
+  );
+}
