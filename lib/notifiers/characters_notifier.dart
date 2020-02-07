87f521f1feaf3c7aa93d3b898f158fcf72e3a3af
Vladimir Ventura <vladventura@outlook.es>
2020-02-06 22:51:07 -0500

    I'm already including the Provider pattern for the application. This will allow us to  isolate the data from wherever it's not needed

---
+++ lib/notifiers/characters_notifier.dart
@@ -0,0 +1,22 @@
+import 'dart:convert' show json;
+
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+class CharacterNotifier with ChangeNotifier {
+  List _characters;
+  List get characters => _characters;
+
+  set characters(List data) {
+    _characters = data;
+    notifyListeners();
+  }
+
+  void initialize() {
+    Map data;
+    rootBundle.loadString('assets/db.json').then((value) {
+      data = json.decode(value);
+      this.characters = data['characters'];
+    });
+  }
+}
