7ed83ab365717ae2b9c6c7422040488e88520dae
Vladimir Ventura <vladventura@outlook.es>
2020-02-14 00:00:41 -0500

    Structuring the card in a stronger path towards the end result because it's time already that this app gets done right?! Enthusiasm!!

--- lib/components/character_card.dart
+++ lib/components/character_card.dart
@@ -7,45 +7,61 @@ Widget characterCard(dynamic data, BuildContext context) {
   CharacterNotifier characterNotifier = Provider.of<CharacterNotifier>(context);
   return Container(
     decoration: BoxDecoration(
-        color: Colors.amber,
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-              blurRadius: 10,
-              offset: Offset.zero,
-              color: Colors.grey.withOpacity(0.5))
-        ],
-        borderRadius: BorderRadius.circular(15)),
+      color: Colors.black38,
+      borderRadius: BorderRadius.circular(15),
+      image: new DecorationImage(
+          image: new AssetImage("assets/Slivers/Byleth_sliver_1.png"),
+          fit: BoxFit.fitWidth),
+    ),
     margin: EdgeInsets.all(10),
-    child: FlatButton(
-      onPressed: () {
-        characterNotifier.selectedCharacter = data;
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => CharacterDisplay()));
-      },
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      padding: EdgeInsets.all(20),
-      child: Stack(
-        children: [
-          new Center(
-              child: new Text(
-            data['series'],
-            style: TextStyle(
-              color: Colors.red,
-            ),
-          )),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Text(data['info']['name']),
-                    Text(data['difficulty'].toString()),
-                  ],
-                ),
-                Text("Stock Icon here"),
-                Text("Custom color"),
-              ])
-        ],
+    child: Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: new Color.fromARGB(150, 0, 0, 0),
+      ),
+      child: FlatButton(
+        onPressed: () {
+          characterNotifier.selectedCharacter = data;
+          Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => CharacterDisplay()));
+        },
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        padding: EdgeInsets.all(20),
+        child: Stack(
+          children: [
+            new Center(
+                child: new Text(
+              data['series'],
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            )),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            data['info']['name'],
+                            style: new TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            "Difficulty: ${data['difficulty'].toString()}",
+                            style: new TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  new Image(
+                    image: new AssetImage(
+                        "assets/Stock/${data['info']['name']}_stock_${0}.png"),
+                  ),
+                ])
+          ],
+        ),
       ),
     ),
   );
