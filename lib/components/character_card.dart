1dac8d865cd356460431222884902a0cce309325
Vladimir Ventura <vladventura@outlook.es>
2020-02-17 11:31:19 -0500

    Adding the series and making the sliver background slightly darker

--- lib/components/character_card.dart
+++ lib/components/character_card.dart
@@ -5,9 +5,10 @@ import 'package:ssbu_info/screens/character_display.dart';
 
 Widget characterCard(dynamic data, BuildContext context) {
   CharacterNotifier characterNotifier = Provider.of<CharacterNotifier>(context);
+
   return Container(
     decoration: BoxDecoration(
-      color: Colors.black38,
+      color: Colors.black45,
       borderRadius: BorderRadius.circular(15),
       image: new DecorationImage(
           image: new AssetImage(
@@ -31,11 +32,10 @@ Widget characterCard(dynamic data, BuildContext context) {
         child: Stack(
           children: [
             new Center(
-                child: new Text(
-              data['series'],
-              style: TextStyle(
-                color: Colors.red,
-              ),
+                child: new Image(
+              image: new AssetImage("assets/Series/${data['series']}.png"),
+              fit: BoxFit.contain,
+              width: 60,
             )),
             Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -58,7 +58,7 @@ Widget characterCard(dynamic data, BuildContext context) {
                   ),
                   new Image(
                     image: new AssetImage(
-                        "assets/Stock/${data['info']['name']}_stock_${0}.png"),
+                        "assets/Stock/${data['info']['name']}_stock_0.png"),
                   ),
                 ])
           ],
