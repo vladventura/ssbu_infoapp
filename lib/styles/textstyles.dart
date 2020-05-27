17f74e1e7d50040d1287ea450694660c7d182780
Vlad Ventura <vladventura@outlook.es>
2020-05-27 01:02:13 -0400

    Adding more styles to other texts

--- lib/styles/textstyles.dart
+++ lib/styles/textstyles.dart
@@ -4,9 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 TextStyle headline6 = GoogleFonts.notoSans(
   color: Colors.red,
   fontSize: 20,
-  letterSpacing: 1.5,
+  letterSpacing: 1.2
 );
 
-TextStyle bodyText1= GoogleFonts.titilliumWeb(
-                  color: Colors.red
-                );
+TextStyle bodyText1 = GoogleFonts.titilliumWeb(color: Colors.red, fontWeight: FontWeight.bold);
+TextStyle bodyText2 = GoogleFonts.karla(color: Colors.white, fontWeight: FontWeight.w500);
