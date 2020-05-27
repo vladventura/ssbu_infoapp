da58052be79dc3a8f10511ed40e9a988ae646d24
Vlad Ventura <vladventura@outlook.es>
2020-05-27 00:16:08 -0400

    Here all the text styles will reside

---
+++ lib/styles/textstyles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle headline6 = GoogleFonts.notoSans(
+  color: Colors.red,
+  fontSize: 20,
+  letterSpacing: 1.5,
+);
+
+TextStyle bodyText1= GoogleFonts.titilliumWeb(
+                  color: Colors.red
+                );
