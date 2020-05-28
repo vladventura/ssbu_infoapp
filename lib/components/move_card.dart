59fd3a03f76a7274b3e0c81ab614537d7856d0ea
Vlad Ventura <vladventura@outlook.es>
2020-05-28 14:54:35 -0400

    Dissecting the build method a little bit. Flutter can get convoluted very fast (mostly because of the language's syntax, I find JSX easier to read).

--- lib/components/move_card.dart
+++ lib/components/move_card.dart
@@ -59,6 +59,82 @@ class _MoveCardState extends State<MoveCard> {
     });
   }
 
+  Positioned descriptionOverlay() {
+    return new Positioned(
+        child: new AnimatedOpacity(
+      opacity: _visibility ? 1.0 : 0.0,
+      duration: new Duration(milliseconds: 100),
+      child: new Container(
+        child: Center(
+          child: new Text(
+            _currentMove,
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        decoration: BoxDecoration(color: Colors.black87),
+      ),
+    ));
+  }
+
+  Positioned bottomBar() {
+    return new Positioned(
+      bottom: 0,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.07,
+      child: Container(
+        decoration: new BoxDecoration(color: Colors.black87),
+        height: MediaQuery.of(context).size.height * 0.07,
+        child: new Row(
+          children: [
+            bottomBarButton(),
+            Spacer(flex: 5),
+            new Text(_currentName, style: TextStyle(color: Colors.white)),
+            Spacer(flex: 1),
+            bottomBarArrows(),
+            Spacer(
+              flex: 1,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Image bottomBarButton() {
+    return new Image(
+      image: new AssetImage(
+        "assets/Controls/" + widget.button + ".png",
+      ),
+      height: MediaQuery.of(context).size.height * 0.3,
+      width: MediaQuery.of(context).size.width * 0.3,
+    );
+  }
+
+  Widget bottomBarArrows() {
+    return (widget.move.length == 1
+        ? new Container()
+        : (_imageIndex < widget.move.length - 1
+            ? new Icon(
+                Icons.arrow_right,
+                color: Colors.white,
+              )
+            : new Icon(Icons.arrow_left, color: Colors.white)));
+  }
+
+  CarouselOptions carouselOptions() {
+    return CarouselOptions(
+        onPageChanged: (page, c) {
+          this.setState(() {
+            _imageIndex = page;
+            _currentMove = widget.move[_imageIndex]['description'];
+            _currentName = widget.move[_imageIndex]['name'];
+          });
+        },
+        autoPlay: false,
+        enableInfiniteScroll: false,
+        viewportFraction: 1.0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -72,70 +148,13 @@ class _MoveCardState extends State<MoveCard> {
           children: [
             new Container(
               child: new CarouselSlider(
-                options: CarouselOptions(
-                    onPageChanged: (page, c) {
-                      this.setState(() {
-                        _imageIndex = page;
-                        _currentMove = widget.move[_imageIndex]['description'];
-                        _currentName = widget.move[_imageIndex]['name'];
-                      });
-                    },
-                    autoPlay: false,
-                    enableInfiniteScroll: false,
-                    viewportFraction: 1.0),
+                options: carouselOptions(),
                 items: _images,
                 carouselController: _carouselController,
               ),
             ),
-            new Positioned(
-              bottom: 0,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.07,
-              child: Container(
-                decoration: new BoxDecoration(color: Colors.black87),
-                height: MediaQuery.of(context).size.height * 0.07,
-                child: new Row(
-                  children: [
-                    new Image(
-                      image: new AssetImage(
-                        "assets/Controls/" + widget.button + ".png",
-                      ),
-                      height: MediaQuery.of(context).size.height * 0.3,
-                      width: MediaQuery.of(context).size.width * 0.3,
-                    ),
-                    Spacer(flex: 5),
-                    new Text(_currentName,
-                        style: TextStyle(color: Colors.white)),
-                    Spacer(flex: 1),
-                    (widget.move.length == 1
-                        ? new Container()
-                        : (_imageIndex < widget.move.length - 1
-                            ? new Icon(
-                                Icons.arrow_right,
-                                color: Colors.white,
-                              )
-                            : new Icon(Icons.arrow_left, color: Colors.white))),
-                    Spacer(
-                      flex: 1,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            new Positioned(
-                child: new AnimatedOpacity(
-              opacity: _visibility ? 1.0 : 0.0,
-              duration: new Duration(milliseconds: 100),
-              child: new Container(
-                child: Center(
-                  child: new Text(
-                    _currentMove,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                decoration: BoxDecoration(color: Colors.black87),
-              ),
-            ))
+            bottomBar(),
+            descriptionOverlay()
           ],
         ),
       ),
